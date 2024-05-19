b1e287df2b7f656f5a2d6a9b57fa3e7b77bf3aa3
long.phan <dragons3232@gmail.com>
2024-05-19 11:12:12 +0700

    Kysely queries

--- function/src/handler/hello.ts
+++ function/src/handler/hello.ts
@@ -1,9 +1,22 @@
+import { db } from '../kysely/database';
 
 export const handle = async (event: any, context: any, callback: Function) => {
   console.log(event, context)
 
   const { body } = event;
   const { name, message } = typeof body === 'string' ? JSON.parse(body) : body;
+
+  await db.insertInto('Log')
+    .values({
+      message: Math.random().toString(36).substring(7),
+    })
+    .executeTakeFirst()
+
+  const logs = await db.selectFrom('Log')
+    .selectAll()
+    .execute()
+  console.log("🚀 ~ handle ~ logs:", logs)
+
   return respond(true, `${message}, ${name}`);
 }
 
